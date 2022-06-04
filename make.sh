a8eb832db5bd24057d8fd13e5bc9a8afff4396ce
corbtastik <corbtastik@gmail.com>
2022-06-04 14:28:26 -0500

    openshift route for todos-webui

--- make.sh
+++ make.sh
@@ -1,37 +1,28 @@
 #!/bin/bash
+NAMESPACE=${NAMESPACE:-todos}
+RELEASE_NAME=${RELEASE_NAME:-todos1}
+
 function todos-webui-template() {
-  NAMESPACE=todos
-  RELEASE_NAME=todos-webui-1
   helm template --namespace $NAMESPACE $RELEASE_NAME src/todos-webui > .rendered/$RELEASE_NAME.yaml
 }
 
 function todos-webui-install() {
-  NAMESPACE=todos
-  RELEASE_NAME=todos-webui-1
   helm install --namespace $NAMESPACE $RELEASE_NAME src/todos-webui
 }
 
 function todos-webui-uninstall() {
-  NAMESPACE=todos
-  RELEASE_NAME=todos-webui-1
   helm uninstall --namespace $NAMESPACE $RELEASE_NAME
 }
 
 function todos-app-template() {
-  NAMESPACE=todos
-  RELEASE_NAME=todos-app-1
   helm template --namespace $NAMESPACE $RELEASE_NAME src/todos-app > .rendered/$RELEASE_NAME.yaml
 }
 
 function todos-app-install() {
-  NAMESPACE=todos
-  RELEASE_NAME=todos-app-1
   helm install --namespace $NAMESPACE $RELEASE_NAME src/todos-app
 }
 
 function todos-app-uninstall() {
-  NAMESPACE=todos
-  RELEASE_NAME=todos-app-1
   helm uninstall --namespace $NAMESPACE $RELEASE_NAME
 }
 
